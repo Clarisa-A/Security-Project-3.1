888300be860614aef470836792c28c85c37e3dd1
Clarisa-A <clarisagu45@gmail.com>
2026-04-06 23:03:55 -0500

    Encryption functions

--- Common/common.cpp
+++ Common/common.cpp
@@ -18,16 +18,17 @@ void encryptMessage(const std::string& plaintext, unsigned char* ciphertext, int
     int len;
 
     // Call the method from OpenSSL to create and initialize the context. Then call the handleErrors() method
-    
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
     // Call the method from OpenSSL to initialize encryption operation. Then call the handleErrors() method
-    
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (const unsigned char*)aes_key.c_str(), iv)) handleErrors();
 
     // Call the method from OpenSSL to encrypt plaintext. Then call the handleErrors() method
-    
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, (const unsigned char*)plaintext.c_str(), plaintext.length())) handleErrors();
+    *ciphertext_len = len;
 
     // Call the method from OpenSSL to finalize encryption. Then call the handleErrors() method
-
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
     
     // Add the final length to the total ciphertext length
     *ciphertext_len += len;
